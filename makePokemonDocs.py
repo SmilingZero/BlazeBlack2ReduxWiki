7fd3acdb0855f5c91724ea31046f1132dec8725c
SmilingZero <146691000+SmilingZero@users.noreply.github.com>
2023-10-05 23:16:17 -0400

    setting up mkdocs things for local testing

--- makePokemonDocs.py
+++ makePokemonDocs.py
@@ -5,4 +5,18 @@ import json
 import csv
 import re
 
+def getint(name):
+    basename = name.partition('.')
+    return int(basename[0])
+
 os.makedirs('docs/pokemons')
+pkmnDir = 'scrapedJSON/pokemon/'
+listOfPokemonFiles = os.listdir(pkmnDir)
+listOfPokemonFiles.sort(key=getint)
+moveList = 'scrapedJSON/ref/moves_with_descriptions.json'
+
+for ind, jsonFile in enumerate(listOfPokemonFiles):
+    with open(pkmnDir+jsonFile) as pkmnFile:
+        fileInformation = json.load(pkmnFile)
+        markdownString = ''
+    print('stop')
--- pokemonDocWriter.py
+++ pokemonDocWriter.py
@@ -5,3 +5,4 @@ import json
 import csv
 import re
 
+
