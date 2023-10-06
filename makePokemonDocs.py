25d883466c95c2799240d46decec4eaac9e89e0b
SmilingZero <146691000+SmilingZero@users.noreply.github.com>
2023-10-06 01:17:12 -0400

    Working on pokemon doc page automation...

--- makePokemonDocs.py
+++ makePokemonDocs.py
@@ -4,19 +4,25 @@ import tqdm
 import json
 import csv
 import re
+import pokemonDocWriter as pkmnWriter
 
 def getint(name):
     basename = name.partition('.')
     return int(basename[0])
 
-os.makedirs('docs/pokemons')
+docOutFolder = 'docs/pokemons/'
+os.makedirs(docOutFolder, exist_ok=True)
 pkmnDir = 'scrapedJSON/pokemon/'
 listOfPokemonFiles = os.listdir(pkmnDir)
 listOfPokemonFiles.sort(key=getint)
 moveList = 'scrapedJSON/ref/moves_with_descriptions.json'
 
+
+
 for ind, jsonFile in enumerate(listOfPokemonFiles):
     with open(pkmnDir+jsonFile) as pkmnFile:
-        fileInformation = json.load(pkmnFile)
-        markdownString = ''
+        pkmnInformation = json.load(pkmnFile)
+    outfilename, markdownText = pkmnWriter.getPokemonMarkdown(pkmnInformation)
+    with open(docOutFolder+outfilename, mode='w') as outputMarkdown:
+        outputMarkdown.write(markdownText)
     print('stop')
--- pokemonDocWriter.py
+++ pokemonDocWriter.py
@@ -5,4 +5,77 @@ import json
 import csv
 import re
 
+def getTypeIncludes():
+    typesFolder = 'docs/img/type/'
+    typeFiles = os.listdir(typesFolder)
+    includeText = ''
+    for i, fname in enumerate(typeFiles):
+        typestring = fname.replace('.png', '')
+        typeInclude = "[{type}]: ../img/type/{fid}\n".format(type=typestring, fid=fname)
+        includeText += typeInclude
+    return includeText
 
+def getSpeciesTypeString(typeList):
+    individualTypeStrings = ['![][{typename}]'.format(typename=s.lower()) for s in typeList]
+    return ' '.join(individualTypeStrings)
+
+def getTopLevelHeader(pkmnInformation):
+    number = pkmnInformation[0]['Number']
+    natDexNumber = f"{number:03}"
+    speciesName = pkmnInformation[0]['Name']
+    formMarkerInd = speciesName.find('-')
+    speciesTypeString = getSpeciesTypeString(pkmnInformation[0]['TYPE'])
+    
+    if formMarkerInd != -1:
+        speciesName = speciesName[:formMarkerInd].strip()
+    if len(pkmnInformation) > 1:
+        headerText = "# {number} - {speciesname}\n".format(number = natDexNumber, speciesname = speciesName)
+    else:
+        headerText = "# {number} - {speciesname} &nbsp; {type}\n".format(number = natDexNumber, speciesname = speciesName, type=speciesTypeString)
+    return headerText
+
+
+def getPokemonMarkdown(pkmnInformation):
+    # Regardless of number of forms, the natDex number is constant...
+    number = pkmnInformation[0]['Number']
+    natDexNumber = f"{number:03}"
+    topLevelHeader = getTopLevelHeader(pkmnInformation)
+    if len(pkmnInformation) > 1:
+        print('Have not implemented')
+    else:
+        bodyText = getSingleFormMarkdown(pkmnInformation[0])
+
+    markdownText = topLevelHeader + bodyText + getTypeIncludes()
+    outfilename = natDexNumber+'.md'
+    return outfilename, markdownText
+
+def getTypeTable(defense):
+    markdownTable = '## Defenses\n'
+    tableHeader = '\n| ' + ' | '.join(defense.keys()) + ' |\n'
+    separator = '| ' + ' | '.join(['---' for i in defense.keys()]) + ' |\n'
+    def getCol(v):
+        if len(v) == 0:
+            return '&nbsp;'
+        else:
+            return '<br>'.join(['![][{type}]'.format(type = i.lower()) for i in v])+'<br>'
+    contentRow = '| ' + ' | '.join([getCol(v) for k,v in defense.items()]) + ' |\n'
+    return markdownTable+tableHeader+separator+contentRow+'\n'
+
+def getSingleFormMarkdown(pkmnInformation):
+    number = pkmnInformation['Number']
+    natDexNumber = f"{number:03}"
+    pkmnInclude, imString = getPokemonImage(number,0)
+
+    bodyText = ''
+    bodyText+=imString
+    bodyText+= getTypeTable(pkmnInformation['Defenses'])
+    bodyText+=pkmnInclude
+    return bodyText
+
+def getPokemonImage(number, form):
+    natDexNumber = f"{number:03}"
+    imString = "![][{nm}]\n".format(nm=natDexNumber)
+    fname = str(number)+'_'+str(form)+'.png'
+    pkmnInclude = "[{nm}]: ../img/pokemon/{fid}\n".format(nm=natDexNumber, fid=fname)
+    return pkmnInclude, imString
+    
