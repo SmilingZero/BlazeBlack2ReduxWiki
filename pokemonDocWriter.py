de9af28225f66693a76a9334cdcf8247f95d60e4
SmilingZero <146691000+SmilingZero@users.noreply.github.com>
2024-06-14 18:28:32 -0400

    Refactored generation of pokemon doc pages

--- pokemonDocWriter.py
+++ pokemonDocWriter.py
@@ -15,7 +15,9 @@ def unnest(d, keys=[]):
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
-
+pokemonImageLookup = './data/speciesImageLookup.json'
+with open(pokemonImageLookup, 'r') as f:
+    speciesImageLookup = json.load(f)
 pokemonIndexFile = './data/pokemonIndexList.csv'
 with open(pokemonIndexFile, 'r', encoding='utf-8-sig') as indexFile:
     reader = csv.reader(indexFile)
@@ -26,10 +28,8 @@ with open(detailedMoveListFile) as f:
 pokemonEncounterLocations = 'scrapedJSON/ref/pokemonEncounters.json'
 with open(pokemonEncounterLocations) as f:
     pokemonEncounters = json.load(f)
-
 with open('locationLinkDict.json', mode = 'r') as f:
     locationLinks = json.load(f)
-
 
 def getTypeIncludes():
     typesFolder = 'docs/img/type/'
@@ -67,11 +67,6 @@ def getDefensesTable(defense):
     contentRow = '| ' + ' | '.join([getCol(v) for k,v in defense.items()]) + ' |\n'
     return tableHeader+separator+contentRow+'\n'
 
-pokemonImageLookup = './data/speciesImageLookup.json'
-with open(pokemonImageLookup, 'r') as f:
-    speciesImageLookup = json.load(f)
-
-
 def getSpecies(mon):
     hyphenInd = mon.find('-')
     if('♂' in mon):
@@ -96,7 +91,6 @@ def getSpecies(mon):
         species = "".join([ c if c.isalnum() else "" for c in species ])
     return species, form
 
-
 def getPokemonImage(name, form):
     num = speciesImageLookup[name.lower()]['NatDexNum']
     natDexNumber = f"{num:03}"
@@ -120,7 +114,6 @@ def getEvolutionSection(evol):
     incl = [includeString(entry) for entry in evol]
     return '\n'.join(incl)+'\n', \
                     '\n'.join(content)+'\n'
-
 
 def getSpriteTypeTable(name, form, type):
     pkmnInclude, imString = getPokemonImage(name,form)
@@ -170,7 +163,6 @@ def getStatTable(l_stats):
             s_color = 'red'
         return f"<td style=\"width:{w}%;align:center;vertical-align: middle;\">{s}<{s_diff_tag} style = \"color:{s_color}\">{d}</{s_diff_tag}></td>"
     
-    
     content = [v for i,v in enumerate(stats.values())]
     content.append(sum(content))
     diff = [v for i,v in enumerate(stat_diff.values())]
@@ -214,7 +206,7 @@ def getTMtable(moveList):
                 'Priority: {prio}. {effect}'.format(prio = tm['Priority'], effect = str(tm['Effect']).replace('\n', '<br>')) + ' |'
         return row
     contentRows = [makeTMRow(data) for i,data in tmData.items()]
-    return '', tableHeader+separator+'\n'.join(contentRows) +'\n\n'
+    return tableHeader+separator+'\n'.join(contentRows) +'\n\n'
 
 
 def getLevelUpTable(moveList):
@@ -239,7 +231,7 @@ def getLevelUpTable(moveList):
                 'Priority: {prio}. {effect}'.format(prio = level['Priority'], effect = str(level['Effect']).replace('\n', '<br>')) + ' |'
         return row
     contentRows = [makeLevelRow(i,data) for i,data in enumerate(lvlUpData)]
-    return '', tableHeader+separator+'\n'.join(contentRows) +'\n\n'
+    return tableHeader+separator+'\n'.join(contentRows) +'\n\n'
 
 
 def getTutorTable(moveList):
@@ -262,7 +254,7 @@ def getTutorTable(moveList):
                 'Priority: {prio}. {effect}'.format(prio = level['Priority'], effect = str(level['Effect']).replace('\n', '<br>')) + ' |'
         return row
     contentRows = [makeLevelRow(data) for i,data in tutorData.items()]
-    return '', tableHeader+separator+'\n'.join(contentRows) +'\n\n'
+    return tableHeader+separator+'\n'.join(contentRows) +'\n\n'
 
 def getPreEvoMoveSection(moveList):
     tableColumns = ['Species', 'Method', 'Move']
@@ -272,35 +264,7 @@ def getPreEvoMoveSection(moveList):
         row = '| {species} | {method} | {move} |'.format(species = entry[0], method = entry[1], move = entry[2])
         return row
     contentRows = [makeRow(data) for data in moveList]
-    return '',  tableHeader+separator+'\n'.join(contentRows) +'\n\n'
-
-def getPokemonMarkdown(pkmnInformation):
-    headerText = getTopLevelHeader(pkmnInformation=pkmnInformation[0])
-    if len(pkmnInformation)==1:
-        bodyIncludes, bodyText = getSingleFormMarkdown(pkmnInformation[0])
-    else:
-        bodyText = ''
-        formIncludes = []
-        for ind,form in enumerate(pkmnInformation):
-            incl, formText = getMultiFormMarkdown(form, ind)
-            bodyText+= formText+'\n'
-            [formIncludes.append(i) for i in incl]
-        tmpIncludes = '\n'.join(formIncludes)
-        bodyIncludes = "\n".join(list(OrderedDict.fromkeys(tmpIncludes.split("\n"))))
-    markdownText = '#' + headerText + '\n' + bodyText
-    includeText = '--8<-- "includes/abilities.md"\n\n' + getTypeIncludes() + bodyIncludes + '\n'
-    
-    encounterIncl, encounterTable = getEncounters(pkmnInformation[0]['Name'])
-    if encounterTable != '':
-        markdownText += '\n## Encounter Locations\n\n'
-        markdownText += encounterTable +'\n'
-        includeText+=encounterIncl + '\n'
-    markdownText += includeText
-    number = pkmnInformation[0]['Number']
-    natDexNumber = f"{number:03}"
-    outfilename = natDexNumber+'.md'
-    linkText = '- {htext}: pokemons/{fname}\n'.format(htext=headerText, fname = outfilename)
-    return linkText, outfilename, markdownText
+    return tableHeader+separator+'\n'.join(contentRows) +'\n\n'
 
 def getEncounters(species):
     if(species != 'Ho-Oh' and '-' in species):
@@ -383,52 +347,13 @@ def getEncounters(species):
     tableString = topRow + separator + ''.join(content)
     return placeInclude,tableString
 
-def getSingleFormMarkdown(pkmnInformation):
-    bodyText = ''
-    bodyIncludes = ''
-    number = pkmnInformation['Number']
-    species, form = getSpecies(pkmnInformation['Name'])
-    pkmnInclude, tableString = getSpriteTypeTable(species,form = 'base', type = pkmnInformation['TYPE'])
-        
-    abilityTable = getAbilityTable(pkmnInformation['Ability'])
-    stats = [pkmnInformation['STATS']]
-    if 'VANILLA STATS' in pkmnInformation.keys():
-        stats.append(pkmnInformation['VANILLA STATS'])
-    bstTable = getStatTable(stats)
-    bodyText+=tableString
-    bodyText+='## Defenses\n\n'
-    bodyText+= getDefensesTable(pkmnInformation['Defenses'])
-    bodyText+= '## Ability\n\n'
-    bodyText+=abilityTable
-    bodyText+='## Stats\n\n'
-    bodyText+=bstTable
-    bodyIncludes+=pkmnInclude
-    if len(pkmnInformation['Items']) > 0:
-        bodyText+= '## Wild Hold Items\n'
-        bodyText += getItemString(pkmnInformation['Items'])
-    if len(pkmnInformation['Level Up Moves']) >0:
-        bodyText += '## Level Up Moves\n'
-        lvlUpIncludes, levelUpTable = getLevelUpTable(pkmnInformation['Level Up Moves'])
-        bodyText+= levelUpTable
-    if len(pkmnInformation['TM Moves']) >0:
-        bodyText += '## TM Moves\n'
-        tmIncludes, tmTable = getTMtable(pkmnInformation['TM Moves'])
-        bodyText+= tmTable
-    if len(pkmnInformation['Tutor Moves']) >0:
-        bodyText += '## Tutor Moves\n'
-        tutorIncludes, tutorTable = getTutorTable(pkmnInformation['Tutor Moves'])
-        bodyText+= tutorTable
-    if len(pkmnInformation['Evolutions'])>0:
-        bodyText += '## Evolution\n'
-        evolInclude, evolString = getEvolutionSection(pkmnInformation['Evolutions'])
-        bodyText += evolString
-        bodyIncludes += evolInclude
-    if 'Pre-Evolution Moves' in pkmnInformation.keys() and len(pkmnInformation['Pre-Evolution Moves'])>0:
-        bodyText += '## Pre-Evolution Moves\n'
-        preEvoIncl, preEvoString = getPreEvoMoveSection(pkmnInformation['Pre-Evolution Moves'])
-        bodyText += preEvoString
-        bodyIncludes += preEvoIncl
-    return bodyIncludes, bodyText
+
+def isValid(item):
+    if(item is None):
+        return False
+    if type(item) is not int and len(item) == 0:
+        return False
+    return True
 
 def getSubsectionHeader(pkmnName):
     speciesName = pkmnName
@@ -440,59 +365,69 @@ def getSubsectionHeader(pkmnName):
     return '## {n}\n\n'.format(n=headerName)
 
 
-def getMultiFormMarkdown(pkmn,form):
-    bodyText = ''
+def makePageText(pokemonInformation):
+    pageSections = []
     bodyIncludes = []
-    number = pkmn['Number']
-    formHeader = getSubsectionHeader(pkmn['Name'])
-    bodyText+=formHeader
-    species, form = getSpecies(pkmn['Name'])
-    pkmnInclude, tableString = getSpriteTypeTable(species,form = form, type = pkmn['TYPE'])
-    bodyText+= tableString
-    bodyIncludes.append(pkmnInclude)
-    def isValid(item):
-        if(item is None):
-            return False
-        if type(item) is not int and len(item) == 0:
-            return False
-        return True
-    formKeys = [f for f in pkmn.keys() if isValid(pkmn[f])]
-    if 'Defenses' in formKeys:
-        bodyText+='### Defenses\n'
-        bodyText+= getDefensesTable(pkmn['Defenses'])
-    if 'Ability' in formKeys:
-        bodyText+='### Ability\n'
-        bodyText+=getAbilityTable(pkmn['Ability'])
-    if 'STATS' in formKeys:
-        bodyText+='### Stats\n'
-        stats = [pkmn['STATS']]
-        if 'VANILLA STATS' in pkmn.keys():
-            stats.append(pkmn['VANILLA STATS'])
-        bstTable = getStatTable(stats)
-        bodyText+= bstTable
-    if 'Items' in formKeys:
-        bodyText+='### Wild Hold Items\n'
-        bodyText+= getItemString(pkmn['Items'])
-    if 'Level Up Moves' in formKeys:
-        bodyText += '### Level Up Moves\n'
-        incl, string = getLevelUpTable(pkmn['Level Up Moves'])
-        bodyText += string
-    if 'TM Moves' in formKeys:
-        bodyText += '### TM Moves\n'
-        incl, string = getTMtable(pkmn['TM Moves'])
-        bodyText += string
-    if 'Tutor Moves' in formKeys:
-        bodyText += '### Tutor Moves\n'
-        incl, string = getTutorTable(pkmn['Tutor Moves'])
-        bodyText += string
-    if 'Evolutions' in formKeys:
-        bodyText += '### Evolutions\n'
-        evolIncl, evolString = getEvolutionSection(pkmn['Evolutions'])
-        bodyText += evolString
-        bodyIncludes.append(evolIncl)
-    if  'Pre-Evolution Moves' in formKeys:
-        bodyText += '### Pre-Evolution Moves\n'
-        preEvoIncl, preEvoString = getPreEvoMoveSection(pkmn['Pre-Evolutions Moves'])
-        bodyText += preEvoString
-        bodyIncludes.append(preEvoIncl)
+    header_prepend = ""    
+    if len(pokemonInformation) > 1:
+        header_prepend = "#"
+    elif len(pokemonInformation) == 0:
+        raise TypeError
+    for pkmn in pokemonInformation:
+        if len(pokemonInformation) > 1:
+             formHeader = getSubsectionHeader(pkmn['Name'])
+             pageSections.append(formHeader)
+        species, form = getSpecies(pkmn['Name'])
+        pkmnInclude, tableString = getSpriteTypeTable(species,form = form, type = pkmn['TYPE'])
+        bodyIncludes.append(pkmnInclude)
+        pageSections.append(tableString)
+        
+        formKeys = [f for f in pkmn.keys() if isValid(pkmn[f])]
+
+        if 'Defenses' in formKeys:
+            pageSections.append(header_prepend+'## Defenses\n' + getDefensesTable(pkmn['Defenses']))
+        if 'Ability' in formKeys:
+            pageSections.append(header_prepend+'## Ability\n' + getAbilityTable(pkmn['Ability']))
+        if 'STATS' in formKeys:
+            stats = [pkmn['STATS']]
+            if 'VANILLA STATS' in pkmn.keys():
+                stats.append(pkmn['VANILLA STATS'])
+            bstTable = getStatTable(stats)
+            pageSections.append(header_prepend+'## Stats\n'+bstTable)
+        if 'Items' in formKeys:
+            pageSections.append(header_prepend+'## Wild Hold Items\n'+getItemString(pkmn['Items']))
+        if 'Level Up  Moves' in formKeys:
+            pageSections.append(header_prepend+'## Level Up Moves\n'+ getLevelUpTable(pkmn['Level Up Moves']))
+        if 'TM Moves' in formKeys:
+            pageSections.append(header_prepend+'## TM Moves\n' + getTMtable(pkmn['TM Moves']))
+        if 'Tutor Moves' in formKeys:
+            pageSections.append(header_prepend+'## Tutor Moves\n' + getTutorTable(pkmn['Tutor Moves']))
+        if 'Evolutions' in formKeys:
+            evolIncl, evolString = getEvolutionSection(pkmn['Evolutions'])
+            pageSections.append(header_prepend+'## Evolutions\n'+evolString)        
+            bodyIncludes.append(evolIncl) 
+        if  'Pre-Evolution Moves' in formKeys:
+            preEvoString = getPreEvoMoveSection(pkmn['Pre-Evolution Moves'])
+            pageSections.append(header_prepend+'## Pre-Evolution Moves\n'+preEvoString)
+    bodyText = '\n'.join(pageSections)
     return bodyIncludes, bodyText
+
+
+def getPokemonMarkdown(pkmnInformation):
+    headerText = getTopLevelHeader(pkmnInformation=pkmnInformation[0])
+    page_includes, page_text = makePageText(pkmnInformation)
+    tmp_incl = '\n'.join(page_includes)
+    body_includes = "\n".join(list(OrderedDict.fromkeys(tmp_incl.split("\n"))))
+    markdownText = '#' + headerText + '\n' + page_text
+    includeText = '--8<-- "includes/abilities.md"\n\n' + getTypeIncludes() + body_includes + '\n'
+    encounterIncl, encounterTable = getEncounters(pkmnInformation[0]['Name'])
+    if encounterTable != '':
+        markdownText += '\n## Encounter Locations\n\n'
+        markdownText += encounterTable +'\n'
+        includeText+=encounterIncl + '\n'
+    markdownText += includeText
+    number = pkmnInformation[0]['Number']
+    natDexNumber = f"{number:03}"
+    outfilename = natDexNumber+'.md'
+    linkText = '- {htext}: pokemons/{fname}\n'.format(htext=headerText, fname = outfilename)
+    return linkText, outfilename, markdownText
